873ec7c8693851dac1c25c3d96231bf17f1381cb
Subhankar Naskar <98633060+Gipsydh@users.noreply.github.com>
2025-07-30 16:20:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       
       <div className='outerBoard'>
-        <h1>Tic Tac Toe Rechecked</h1>
+        <h1>Tic Tac Toe Rechecked again</h1>
         <Board></Board>
       </div>
     </div>
